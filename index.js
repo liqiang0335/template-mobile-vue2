b076e4bbbb88b987038029814d6e3aaa9d879217
liqiang0335@qq.com <mm020318@2>
2018-08-13 13:10:08 +0800

    20180813131008

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import "@/style/common.scss";
 import Vue from "vue";
 import router from "./router";
 import store from "./store";
+import App from "./App";
 
 Vue.config.productionTip = false;
 
@@ -15,8 +16,6 @@ import myToast from "ynw/vue/plugin/web";
 Vue.use(myToast);
 
 importVueComps(); //导入comps文件夹下的组件
-
-import App from "./App";
 
 new Vue({
   el: "#app",
