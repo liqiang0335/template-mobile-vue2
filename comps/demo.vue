66f0d737e6046f93ab1d294e6fd9aad68e93553f
liqiang0335@qq.com <mm020318@2>
2018-08-13 15:19:16 +0800

    20180813151916

--- comps/demo.vue
+++ comps/demo.vue
@@ -3,6 +3,18 @@
     <h1>Header</h1>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted() {}
+};
+</script>
 <style lang="scss" scoped>
 @import "~@/style/common.scss";
 </style>
+
