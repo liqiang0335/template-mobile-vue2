12bb2aa147ddf41c2a770aa26a11c2011d91d038
liqiang0335@qq.com <liqiang0335@gmail.com>
2018-08-13 23:51:41 +0800

    20180813235139

--- script/plugin.js
+++ script/plugin.js
@@ -1,14 +1,19 @@
 import { Message, MessageBox } from "element-ui";
 import pick from "ynw/object/pick";
 import extend from "ynw/object/extend";
+import * as api from "@/script/api";
+import * as config from "@/script/config";
+import * as Types from "@/script/types";
 
 export default {
   install(Vue, option) {
+    Vue.prototype.$config = config;
+    Vue.prototype.$types = Types;
+    Vue.prototype.$api = api;
     Vue.prototype.$pick = pick;
     Vue.prototype.$extend = extend;
     Vue.prototype.$error = message => Message({ type: "error", message });
     Vue.prototype.$success = message => Message({ type: "success", message });
-    Vue.prototype.$warning = message => Message({ type: "warning", message });
     Vue.prototype.$confirm = message =>
       MessageBox.confirm(message, "提示", {
         confirmButtonText: "确定",
