669c248a27af0127b9ed1b96d25415af0cee600a
liqiang0335@qq.com <mm020318@2>
2018-08-13 15:37:08 +0800

    20180813153707

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ import "ynw/style/reset.css";
 import "@/style/common.scss";
 
 import Vue from "vue";
-import router from "./router";
-import store from "./store";
 import App from "./App";
-
 Vue.config.productionTip = false;
 
 import myToast from "ynw/vue/plugin/web";
 Vue.use(myToast);
 
-importVueComps(); //导入comps文件夹下的组件
+importVueComps();
+
+// import router from "./router";
+// import store from "./store";
 
 new Vue({
   el: "#app",
-  router,
-  store,
+  // router,
+  // store,
   render: h => h(App)
 });
