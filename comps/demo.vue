e6175e234ec24de982db8a7aadf0101cf229e178
liqiang0335@qq.com <liqiang0335@gmail.com>
2018-08-22 00:15:17 +0800

    a20180822

--- comps/demo.vue
+++ comps/demo.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <h1>Header</h1>
+    <input type="text" v-model="phone" placeholder="请输入手机号">
+    <button @click="submit">提交</button>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      phone: ""
+    };
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    async submit() {
+      //表单验证
+      await this.$fn.validate({
+        value: this.phone.trim(),
+        key: "phone"
+      });
+
+      //设置State
+      this.$setState("userId", 100);
+    }
+  },
   mounted() {}
 };
 </script>
