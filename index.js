2e78f3102c390d6486e4ba4199ccbc38eed8e9d4
liqiang <liqiang0335@qq.com>
2019-01-04 14:35:19 +0800

    add

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Promise
 import { Promise } from "es6-promise";
 window.Promise = Promise;
 
@@ -6,23 +5,14 @@ import promiseFinally from "promise.prototype.finally";
 promiseFinally.shim();
 
 import "ynw/style/reset.css";
-import "@/style/common.scss";
+import "./style/common.scss";
 
 import Vue from "vue";
 import App from "./App";
 
 Vue.config.productionTip = false;
 
-import "./script/register";
-import plugin from "./script/plugin";
-Vue.use(plugin);
-
-// import router from "./router";
-// import store from "./store";
-
 new Vue({
   el: "#app",
-  // router,
-  // store,
   render: h => h(App)
 });
---
+++ script/hub.js
@@ -0,0 +1,3 @@
+import { EventHub } from "ynw-utils";
+const hub = new EventHub();
+export default hub;
