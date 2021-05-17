78f75bb4be080309a448b711a4bae5bb19ca26c2
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-05-17 20:56:53 +0800

    add

---
+++ script/vant.js
@@ -0,0 +1,93 @@
+import Vue from "vue";
+
+Vue.config.productionTip = false;
+
+import {
+  Tag,
+  Swipe,
+  SwipeItem,
+  DropdownMenu,
+  DropdownItem,
+  Collapse,
+  CollapseItem,
+  Grid,
+  GridItem,
+  Empty,
+  Loading,
+  ImagePreview,
+  Row,
+  Col,
+  Lazyload,
+  Divider,
+  Notify,
+  Image,
+  Dialog,
+  PullRefresh,
+  Toast,
+  CheckboxGroup,
+  Checkbox,
+  Stepper,
+  DatetimePicker,
+  Popup,
+  Picker,
+  SwitchCell,
+  RadioGroup,
+  Radio,
+  Switch,
+  Button,
+  Field,
+  NavBar,
+  Tab,
+  Tabs,
+  Icon,
+  Tabbar,
+  TabbarItem,
+  Uploader,
+  Cell,
+  CellGroup,
+  List,
+} from "vant";
+
+Vue.use(Tag);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(Empty);
+Vue.use(Grid);
+Vue.use(GridItem);
+Vue.use(Loading);
+Vue.use(ImagePreview);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Lazyload);
+Vue.use(Divider);
+Vue.use(Notify);
+Vue.use(Image);
+Vue.use(Dialog);
+Vue.use(PullRefresh);
+Vue.use(Toast);
+Vue.use(Stepper);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(DatetimePicker);
+Vue.use(Popup);
+Vue.use(Picker);
+Vue.use(SwitchCell);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(Button);
+Vue.use(Switch);
+Vue.use(Field);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(List);
+Vue.use(Tab);
+Vue.use(Tabs);
+Vue.use(NavBar);
+Vue.use(Icon);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Uploader);
