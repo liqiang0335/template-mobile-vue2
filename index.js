4abfc322f17c3ba2e0e6828b8d31bf255521a345
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-07-25 17:19:44 +0800

    add

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import store from "./store/index";
+import plugin from "./script/plugin";
 
 import "./style/reset.css";
 import "./style/common.css";
 import "./script/register";
 import "./script/vant";
 
+Vue.use(plugin);
 Vue.config.productionTip = false;
 
 new Vue({
--- script/plugin.js
+++ script/plugin.js
@@ -1,19 +1,37 @@
-import { Message, MessageBox } from "element-ui";
+/**
+ * ----------------------------------------
+ * 校验表单: 默认检验 this.form
+ * 支持校验输入的参数对象
+ * ----------------------------------------
+ */
+const ynvalidate = function (target) {
+  return new Promise((resolve, reject) => {
+    const _target = target || this.form;
+    for (let key in _target) {
+      const value = _target[key];
+      if ((value === "" || value === undefined) && this.errors[key]) {
+        this.$notify(this.errors[key]);
+        return reject("@表单校验失败");
+      }
+    }
+    return resolve();
+  });
+};
+/**
+ * ----------------------------------------
+ * 操作确认
+ * ----------------------------------------
+ */
+const ynconfirm = function (message = "确定要提交吗") {
+  return this.$dialog.confirm({
+    title: "操作确认",
+    message: message,
+  });
+};
 
 export default {
-  install(Vue, option) {
-    Vue.prototype.$error = message => Message({ type: "error", message });
-    Vue.prototype.$success = message => Message({ type: "success", message });
-
-    Vue.prototype.$confirm = message =>
-      MessageBox.confirm(message, "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      });
-
-    // Vue.mixin({
-    //   methods: {},
-    // });
+  install(Vue) {
+    Vue.prototype.ynvalidate = ynvalidate;
+    Vue.prototype.ynconfirm = ynconfirm;
   },
 };
