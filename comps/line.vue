15b6b179815e365e351d2c95dc7e5f0e591be7c1
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-07-25 20:57:22 +0800

    add

---
+++ comps/line.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="aline flex">
+    <div class="label">{{ label }}</div>
+    <div class="value"></div>
+    <slot>--</slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ["label"],
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style scoped lang='scss'>
+.aline {
+  padding: 12px 30px;
+  background: white;
+  align-items: center;
+  margin-bottom: 1px;
+}
+.label {
+  margin-right: 2px;
+  min-width: 30px;
+}
+.value {
+  flex-grow: 1;
+  word-break: break-all;
+}
+</style>
